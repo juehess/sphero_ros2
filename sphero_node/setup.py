912ddefee91302b7b4b68c677015946038c13eac
Juergen Hess <juergenmhess@gmail.com>
2021-11-27 20:19:25 +0100

    Changed executable name

--- sphero_node/setup.py
+++ sphero_node/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'talker = sphero_node.sphero_node:main',
+            'sphero = sphero_node.sphero_node:main',
         ],
     },
 )
