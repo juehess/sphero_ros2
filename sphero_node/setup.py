5b12e459242cf8458f4d75cb024cfb0167f22bcc
Juergen Hess <juergenmhess@gmail.com>
2021-12-03 19:07:45 +0100

    Added licencse infor

--- sphero_node/setup.py
+++ sphero_node/setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 package_name = 'sphero_node'
+transformations = "sphero_node/transformations"
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=[package_name, transformations],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -13,14 +14,15 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='juergen',
-    maintainer_email='juergen@todo.todo',
+    maintainer='Juergen Hess',
+    maintainer_email='juergenmhess@gmail.com',
     description='TODO: Package description',
-    license='TODO: License declaration',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'sphero = sphero_node.sphero_node:main',
+            'sphero2 = sphero_node.sphero_node2:main',
         ],
     },
 )
